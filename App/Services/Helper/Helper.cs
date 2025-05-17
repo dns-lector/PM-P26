5674421336a407062172a8fd4b759636f31f5d2e
dns-lector <157026463+dns-lector@users.noreply.github.com>
2025-05-17 17:06:48 +0300

    TDD

---
+++ App/Services/Helper/Helper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace App.Services.Helper
+{
+    public class Helper
+    {
+        // public String PathCombine(String dir, String file) => "/dir1/file.ext";
+        // public String PathCombine(params String[] parts)
+        // {
+        //     char[] chars = ['/', '\\'];
+        //     return String.Join("",
+        //         parts.Select(part => part.Contains("://")
+        //             ? part
+        //             : $"/{part.TrimEnd(chars).TrimStart(chars)}"));            
+        // }
+        public String PathCombine(params String[] parts)
+        {
+            char[] chars = ['/', '\\'];
+            LinkedList<String> list = [];
+            foreach (String path in parts)
+            foreach (String part in Regex.Split(path, @"(?<!/)/(?!/)"))
+            {
+                if (part == "") continue;
+                if (part == "..") list.RemoveLast();
+                else list.AddLast(
+                    part.Contains("://")
+                    ? part
+                    : $"/{part.TrimEnd(chars).TrimStart(chars)}"
+                );
+            }
+            return String.Join("", list);
+        }
+    }
+}
+/* Д.З. Продовжити впровадження модульних тестів до 
+ * курсового проєкту. Підготуватись до демонстрації проєктів.
+ */
+/*
+ 1 2 3 4 5
+ ""       | 
+ "1"      | garbage
+ "12"     | N * N/2  -- O(N^2)
+ "123"    | 
+ "1234"   | 
+ "12345"
+ */
---
+++ UnitTests/Services/Helper/HelperTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Services.Helper
+{
+    [TestClass]
+    public sealed class HelperTest
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var helper = new App.Services.Helper.Helper();
+        }
+
+        [TestMethod]
+        public void PathCombineTest()
+        {
+            var helper = new App.Services.Helper.Helper();
+            Assert.AreEqual(
+                "/dir1/file.ext",
+                helper.PathCombine("/dir1", "file.ext"),
+                "Combination rule: '/dir1'  + 'file.ext' -> '/dir1/file.ext'"
+            );
+            Assert.AreEqual(
+                "/dir2/file2.ext",
+                helper.PathCombine("/dir2", "/file2.ext"),
+                "Combination rule: '/dir2'  + '/file2.ext' -> '/dir2/file2.ext'"
+            );
+            Assert.AreEqual(
+                "/subdir3/file3.ext",
+                helper.PathCombine("/subdir3/", "//file3.ext"),
+                "Combination rule: '/subdir3/'  + '//file3.ext' -> '/subdir3/file3.ext'"
+            );
+            Assert.AreEqual(
+                "/dir/subdir/file.ext",
+                helper.PathCombine("dir", "subdir", "file.ext"),
+                "Combination rule: 'dir' + 'subdir'  + 'file.ext' -> '/dir/subdir/file.ext'"
+            );
+            Assert.AreEqual(
+                "/dir/subdir1/subdir2/subdir3/file.ext",
+                helper.PathCombine("/dir/", "/subdir1/", "/subdir2/", "/subdir3/", "/file.ext"),
+                "Combination rule: '/dir/' + '/subdir1/' + '/subdir2/' + '/subdir3/'  + '/file.ext' -> '/dir/subdir/file.ext'"
+            );
+
+            Assert.AreEqual(
+                "/dir2/file2.ext",
+                helper.PathCombine(@"\dir2", @"\file2.ext"),
+                @"Combination rule: '\dir2'  + '\file2.ext' -> '/dir2/file2.ext'"
+            );
+            Assert.AreEqual(
+                @"C://dir/file",
+                helper.PathCombine(@"C://dir/file"),
+                @"Combination rule: 'C://dir/file' -> 'C://dir/file'"
+            );
+            Assert.AreEqual(
+                @"/file",
+                helper.PathCombine(@"file"),
+                @"Combination rule: 'file' -> '/file'"
+            );
+            Assert.AreEqual(
+                @"C://dir/file",
+                helper.PathCombine(@"C://dir", "file"),
+                @"Combination rule: 'C://dir' + 'file' -> 'C://dir/file'"
+            );
+
+            Assert.AreEqual(
+                @"/file",
+                helper.PathCombine(@"dir", "..", "file"),
+                @"Combination rule: 'dir' + '..' + 'file' -> '/file'"
+            );
+            Assert.AreEqual(
+                @"/dir2/file",
+                helper.PathCombine(@"dir", "../dir2", "file"),
+                @"Combination rule: 'dir' + '../dir2' + 'file' -> '/dir2/file'"
+            );
+            Assert.AreEqual(
+                @"/dir/dir2/file",
+                helper.PathCombine(@"dir/dir1", "../dir2", "file"),
+                @"Combination rule: 'dir/dir1' + '../dir2' + 'file' -> '/dir/dir2/file'"
+            );
+
+
+
+            // Assert.AreEqual(
+            //     @"C://file",
+            //     helper.PathCombine(@"C://dir", "..", "file"),
+            //     @"Combination rule: 'C://dir' + 'file' -> 'C://dir/file'"
+            // );
+        }
+    }
+}
